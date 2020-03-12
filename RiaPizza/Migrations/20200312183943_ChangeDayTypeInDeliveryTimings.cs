dc7a1920f8513dbccb38ce68f9b33ec9a2b87cce
Tayyab <devtayyabzia@gmail.com>
2020-03-12 23:45:31 +0500

    Delivery timings in info button in dishes page

---
+++ RiaPizza/Migrations/20200312183943_ChangeDayTypeInDeliveryTimings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RiaPizza.Migrations
+{
+    public partial class ChangeDayTypeInDeliveryTimings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Day",
+                table: "DeliveryTimings",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Day",
+                table: "DeliveryTimings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
