f2e39fd001311fea8a228faf026f764ae76836f9
bilal_sql <bilal.sql48@gmail.com>
2020-03-17 20:09:25 +0500

    Coupon work(services, model,controller)Bilal

---
+++ RiaPizza/Models/Coupon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiaPizza.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Decimal DiscountPercent { get; set; }
+        public DateTime ValidityFrom { get; set; }
+        public DateTime ValidityTo { get; set; }
+        public bool Status { get; set; }
+        public bool IsExpired { get; set; }
+    }
+}
